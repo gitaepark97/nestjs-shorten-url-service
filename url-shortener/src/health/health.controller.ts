e042d372531551c62a6ba37316dfb47f6808a1e2
gitaepark97 <parkkitae7@gmail.com>
2024-04-29 20:31:11 +0900

    fix: 불필요한 코드 제거

--- url-shortener/src/health/health.controller.ts
+++ url-shortener/src/health/health.controller.ts
@@ -1,13 +1,6 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
-import { IsNumber } from 'class-validator';
-import { ResponseValidationInterceptor } from 'src/common/validation/response-validation.interceptor';
-
-class Test {
-  @IsNumber()
-  status: number;
-}
 
 @ApiTags('Health')
 @Controller('health')
@@ -16,7 +9,6 @@ export class HealthController {
 
   @Get()
   @HealthCheck()
-  @UseInterceptors(new ResponseValidationInterceptor(Test))
   check() {
     return this.health.check([]);
   }
