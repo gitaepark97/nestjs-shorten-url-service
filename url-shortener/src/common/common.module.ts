e5156efbcabb99070e4890452cfce58fdf06bae6
gitaepark97 <parkkitae7@gmail.com>
2024-05-02 22:19:11 +0900

    style: health 위치 변경

--- url-shortener/src/common/common.module.ts
+++ url-shortener/src/common/common.module.ts
@@ -1,10 +1,14 @@
 import { Logger, Module } from '@nestjs/common';
 import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { TerminusModule } from '@nestjs/terminus';
 import { HttpExceptionFilter } from './exception/http-exception.filter';
+import { HealthController } from './health/health.controller';
 import { LoggingInterceptor } from './logging/logging.interceptor';
 import { RequestValidationPipe } from './validation/request-validatation.pipe';
 
 @Module({
+  imports: [TerminusModule],
+  controllers: [HealthController],
   providers: [
     Logger,
     { provide: APP_PIPE, useClass: RequestValidationPipe },
