d0f441b8155a2466b10b43999d3c3043f9ed2b29
gitaepark97 <parkkitae7@gmail.com>
2024-05-17 11:16:14 +0900

    feat: Count 관리 변경
    DB touch 감소

--- was/src/shorten-url/adapter/out/persistence/count.adapter.ts
+++ was/src/shorten-url/adapter/out/persistence/count.adapter.ts
@@ -2,21 +2,47 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { LoadAndUpdateCountPort } from 'src/shorten-url/application/port/out/load-and-update-count.port';
+import { Mutex } from 'src/util/mutex.util';
 import { CountEntity } from './entity/count.entity';
 
 @Injectable()
 export class CountAdapter implements LoadAndUpdateCountPort {
+  static COUNT_RANGE = 10000;
+
+  private mutex = new Mutex();
+  private count = {
+    first: 0,
+    current: 0,
+    end: 0,
+  };
+
   constructor(
     @InjectModel(CountEntity.name)
     private readonly countModel: Model<CountEntity>,
   ) {}
 
   async findCountAndIncrease(): Promise<number> {
+    const release = await this.mutex.acquire();
+    try {
+      if (this.count.current === this.count.end) {
+        await this.setCount();
+      } else {
+        this.count.current++;
+      }
+    } finally {
+      release();
+    }
+
+    return this.count.current;
+  }
+
+  private async setCount() {
     const count = await this.countModel.findOneAndUpdate(
       {},
-      { $inc: { current: 1 } },
+      { $inc: { current: CountAdapter.COUNT_RANGE } },
     );
-
-    return count!.current;
+    this.count.first = count!.current;
+    this.count.current = this.count.first;
+    this.count.end = this.count.first + CountAdapter.COUNT_RANGE - 1;
   }
 }
