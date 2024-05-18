9e0287fd8bb44bb49220d3f2015faa299fd75075
gitaepark97 <parkkitae7@gmail.com>
2024-05-18 20:14:31 +0900

    feat: 부하 테스트

---
+++ test/load/create-shorten-url.js
@@ -0,0 +1,25 @@
+import http from "k6/http";
+
+export const options = {
+  stages: [
+    { duration: "5m", target: 100 },
+    { duration: "5m", target: 1000 },
+  ],
+};
+
+const BASE_URL = "http://localhost:8000/api/v1";
+
+export default function () {
+  const postUrl = `${BASE_URL}/shorten-urls`;
+
+  const payload = JSON.stringify({
+    originalUrl: "https://www.google.co.kr",
+  });
+  const postParams = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  http.post(postUrl, payload, postParams);
+}
---
+++ test/load/get-shorten-urls.js
@@ -0,0 +1,26 @@
+import { check } from "k6";
+import http from "k6/http";
+
+export const options = {
+  stages: [
+    { duration: "5m", target: 10 },
+    { duration: "5m", target: 50 },
+  ],
+};
+
+const BASE_URL = "http://localhost:8000/api/v1";
+
+export default function () {
+  const url = `${BASE_URL}/shorten-urls?pageNumber=10`;
+
+  const params = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const response = http.get(url, params);
+  check(response, {
+    success: (res) => res.status === 200,
+  });
+}
