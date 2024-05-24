1a17d6a715bee15553af5c24a7305aa3937caf9a
gitaepark97 <parkkitae7@gmail.com>
2024-05-24 13:41:27 +0900

    fix: Logger Inject

--- was/src/shorten-url/adapter/out/mq/shorten-url.producer.ts
+++ was/src/shorten-url/adapter/out/mq/shorten-url.producer.ts
@@ -1,13 +1,10 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ProducerService } from 'src/kafka/producer.service';
 import { UpdateShortenUrlPort } from 'src/shorten-url/application/port/out/update-shorten-url.port';
 
 @Injectable()
 export class ShortenUrlProducer implements UpdateShortenUrlPort {
-  constructor(
-    private readonly producerService: ProducerService,
-    private readonly logger: Logger,
-  ) {}
+  constructor(private readonly producerService: ProducerService) {}
 
   async increaseVisitCountByKey(shortenUrlKey: string): Promise<void> {
     await this.producerService.send({
